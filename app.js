ce2541c39f671818ae3c8943c37979b1289b0d43
baburali <baburali1910@gmail.com>
2024-08-07 03:20:54 -0700

    changed

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log("hello todo");
+
+const list = document.querySelector("#list")
+const input = document.querySelector("#todo-task")
+
+let arr = [];
+function submitData() {
+    
+   if (input.value === '') {
+    alert("enter your todo task")
+    
+   }else{
+    arr.push(input.value)
+    console.log(arr);
+    input.value = ''
+    renderData()
+   }
+    
+    
+}
+
+function renderData() {
+    list.innerHTML = ''
+    for (let i = 0; i < arr.length; i++) {
+        list.innerHTML += `<li>${arr[i]}
+        <button onclick = "deleteData(${i})">delete</button> 
+        <button onclick = "updateData(${i})">edit</button></li>` 
+        
+    }
+}
+
+
+function deleteData(index) {
+    arr.splice(index, 1)
+    renderData()
+}
+function updateData(index) {
+    let updateVal = prompt("enter updated value")
+    arr.splice(index, 1, updateVal )
+    renderData()
+}
+
