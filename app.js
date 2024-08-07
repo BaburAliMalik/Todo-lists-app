134ad6d124f5c4467bf138039cb471cd414ffdcf
baburali <baburali1910@gmail.com>
2024-08-07 04:07:30 -0700

    changed

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-console.log("hello todo");
+// console.log("hello todo");
 
-const list = document.querySelector("#list")
-const input = document.querySelector("#todo-task")
+// const list = document.querySelector("#list")
+// const input = document.querySelector("#add-task")
 
-let arr = [];
-function submitData() {
+// let arr = [];
+// function submitData() {
     
-   if (input.value === '') {
-    alert("enter your todo task")
+//    if (input.value === '') {
+//     alert("enter your todo task")
     
-   }else{
-    arr.push(input.value)
-    console.log(arr);
-    input.value = ''
-    renderData()
-   }
+//    }else{
+//     arr.push(input.value)
+//     console.log(arr);
+//     input.value = ''
+//     renderData()
+//    }
     
     
-}
+// }
 
-function renderData() {
-    list.innerHTML = ''
-    for (let i = 0; i < arr.length; i++) {
-        list.innerHTML += `<li>${arr[i]}
-        <button onclick = "deleteData(${i})">delete</button> 
-        <button onclick = "updateData(${i})">edit</button></li>` 
+// function renderData() {
+//     list.innerHTML = ''
+//     for (let i = 0; i < arr.length; i++) {
+//         list.innerHTML += `<li>${arr[i]}
+//         <button onclick = "deleteData(${i})">delete</button> 
+//         <button onclick = "updateData(${i})">edit</button></li>` 
         
-    }
-}
+//     }
+// }
 
 
-function deleteData(index) {
-    arr.splice(index, 1)
-    renderData()
-}
-function updateData(index) {
-    let updateVal = prompt("enter updated value")
-    arr.splice(index, 1, updateVal )
-    renderData()
-}
+// function deleteData(index) {
+//     arr.splice(index, 1)
+//     renderData()
+// }
+// function updateData(index) {
+//     let updateVal = prompt("enter updated value")
+//     arr.splice(index, 1, updateVal )
+//     renderData()
+// }
+
 
